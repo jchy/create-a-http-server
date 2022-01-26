305ea4f68be46fe12babf2d7c55b095e89b97827
jchy <recjaswantcse@gmail.com>
2022-01-26 15:50:43 +0530

    create http server

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const http = require('http');
-const sum = require('./sum');
+// const sum = require('./sum');
 
 const server = http.createServer((req, res) => {
     try {
-        const a = 2;
-        const b = 3;
         res.writeHead(200, {'Content-Type': 'application/json'});
         const message = "Sum of a and b is ";
-        console.log(message, sum(a, b));
-        res.end(JSON.stringify(sum(a, b),message));
+        res.end(JSON.stringify({"data":"Hello World"}));
     }
     catch (err) {
         console.error("There is some error in Executing sum method",err);
